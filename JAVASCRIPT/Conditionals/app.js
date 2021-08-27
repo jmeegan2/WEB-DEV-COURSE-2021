ac5792d3e5b35b4c38780d104cfe4f2fc0cd6bde
James Meegan <jmeegan2@owlmail.harford.edu>
2021-08-27 01:21:23 -0400

    working on condtionals in javascript

---
+++ JAVASCRIPT/Conditionals/app.js
@@ -0,0 +1,39 @@
+
+// let random = Math.random();
+// if(random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!")
+// }else {
+//     console.log("Your number is greater than 0.5")
+// }
+// console.log(random);
+// const dayOfWeek = prompt("Enter A DAY").toLowerCase();
+
+// if(dayOfWeek === 'monday'){
+//     console.log("UGHHH I HATE MONDAYS!")
+// } else if (dayOfWeek === 'saturday') {
+//     console.log("YAY I LOVE SATURDAYS")
+// } else if (dayOfWeek === 'friday') {
+//     console.log("Fridays are decent especially after work")
+// } else {
+//     console.log("MEH")
+// }
+
+// const age = 89;
+
+// if(age < 5) {
+//     console.log("You are a baby, you get in for free")
+// } else if (age < 10) {
+//     console.log("You are a child, you pay 10 dollars")
+// } else if (age < 65) {
+//     console.log("You are an adult. You Pay $20")
+// } else {
+//     console.log("You are a senior. You pay $10")
+// }
+
+const password = prompt("please enter a new password");
+
+// Password must be 6+ characters
+if(password.length >= 6) {
+    console.log("long enough password")
+}
+// Password cannot include space
