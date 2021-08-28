da33c99fc67d724782d67c8ed7d2d8accd39a4bc
James Meegan <jmeegan2@owlmail.harford.edu>
2021-08-27 20:11:11 -0400

    switch javascript

---
+++ JAVASCRIPT/Switch/app.js
@@ -0,0 +1,52 @@
+const day = 9;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+    case 7:
+        console.log("WEEKEND");
+        break;
+    default:
+        console.log("I DONT KNOW THAT!!")
+
+}
+
+
+
+// if(day === 1){
+//     console.log("MONDAY")
+// }
+// else if(day === 2) {
+//     console.log("TUESDAY")
+// }
+// else if(day === 3) {
+//     console.log("Wednesday")
+// }
+// else if(day === 4) {
+//     console.log("Thursday")
+// } 
+// else if(day === 5) {
+//     console.log("Friday")
+// }
+// else if(day === 6) {
+//     console.log("Saturday")
+// }
+// else if(day === 7) {
+//     console.log("Sunday")
+// }
+// else { 
+//     console.log("I DONT KNOW THAT!")
+// }
