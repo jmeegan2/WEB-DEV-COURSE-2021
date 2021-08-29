b8e5d645615748f76915ba52c167a6491aaed7c0
James Meegan <jmeegan2@owlmail.harford.edu>
2021-08-28 20:16:20 -0400

    iterating objects

---
+++ JAVASCRIPT/For of Loops/app.js
@@ -0,0 +1,32 @@
+// const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics']
+
+// for (let i = 0; i < subreddits.length; i++) {
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`)
+// }
+
+// for (let sub of subreddits) {
+//     console.log(sub)
+// }
+
+// const seatingChart = [
+//     ['dark', 'bark', 'lark'],
+//     ['fark', 'kark', 'Park'],
+//     ['mark', 'shark', 'Cark']
+// ]
+
+// for (let i = 0; i < seatingChart.length; i++) {
+//     const row = seatingChart[i];
+//     for (let j = 0; j < row.length; j++) {
+//         console.log(row[j])
+//     }
+// }
+
+// for (let row of seatingChart) {
+//     for (let student of row) {
+//         console.log(student);
+//     }
+// }
+
+// for (let char of "hello world") {
+//     console.log(char)
+// }
