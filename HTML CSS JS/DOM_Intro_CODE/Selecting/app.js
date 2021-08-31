3098f36e34ecbc900fe1e2d586c7bc40ae53974d
James Meegan <jmeegan2@owlmail.harford.edu>
2021-08-31 15:02:48 -0400

    manipulating dom

--- HTML CSS JS/DOM_Intro_CODE/Selecting/app.js
+++ HTML CSS JS/DOM_Intro_CODE/Selecting/app.js
@@ -17,8 +17,26 @@
 //     console.log(link.href)
 // }
 
+// const squareImages = document.getElementsByClassName('square');
+
+// (let img of squareImages) {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+// }
+
+// const allImages = document.getElementsByTagName('img')
+
+// for(let img of allImages){
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+// }
+
 const squareImages = document.getElementsByClassName('square');
 
-(let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+for(let img of squareImages){
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/A_fuzzy_baby_chicken_and_its_mom.jpg/220px-A_fuzzy_baby_chicken_and_its_mom.jpg'
+}
+
+const links = document.querySelectorAll('p a')
+
+for (let link of links) {
+    console.log(link.href)
 }
