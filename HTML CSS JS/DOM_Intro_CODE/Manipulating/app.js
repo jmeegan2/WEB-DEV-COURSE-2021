fdc1c340f34914942fbd0553dba26d7854710468
James Meegan <jmeegan2@owlmail.harford.edu>
2021-09-01 19:32:09 -0400

    tons of javascript also a small pokemon demo

--- HTML CSS JS/DOM_Intro_CODE/Manipulating/app.js
+++ HTML CSS JS/DOM_Intro_CODE/Manipulating/app.js
@@ -14,11 +14,18 @@ const allLinks = document.querySelectorAll('a');
 // const allLinks = document.querySelectorAll('a')
 
 // for(let link of allLinks){
-//     link.innerText = 'I AM A LINK'
-// }
+// //     link.innerText = 'I AM A LINK'
+// // }
 
-for(let link of allLinks) {
-    link.style.color = 'rgb(0,108,134)'
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
-    }
+// for(let link of allLinks) {
+//     link.style.color = 'rgb(0,108,134)'
+//     link.style.textDecorationColor = 'magenta';
+//     link.style.textDecorationStyle = 'wavy'
+//     }
+
+// let i = 1
+// let body = document.getElementsByTagName("body")[0]
+// for (i; i <= 100; i++)
+// let button = document.createElement("button");
+// button.innerHTML ='Button '+i;
+// body.appendChild(button);
