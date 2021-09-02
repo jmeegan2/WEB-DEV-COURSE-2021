1c72355c52e5334b116acf09ce7e5a5af5ef223f
James Meegan <jmeegan2@owlmail.harford.edu>
2021-09-01 21:25:17 -0400

    done for today, been working on eventListener stuf

--- HTML CSS JS/DOM_Intro_CODE/DOM_Events_CODE/EventsAndThis/app.js
+++ HTML CSS JS/DOM_Intro_CODE/DOM_Events_CODE/EventsAndThis/app.js
@@ -8,15 +8,43 @@ const makeRandColor = () => {
 const buttons = document.querySelectorAll('button');
 
 for (let button of buttons) {
-    button.addEventListener('click', colorize)
+    button.addEventListener('click',function () {
+        button.style.backgroundColor = makeRandColor();
+        button.style.color = makeRandColor();
+    })
 }
 
 const h1s = document.querySelectorAll('h1');
-for (let h1 of h1s) {
-    h1.addEventListener('click', colorize)
+for(let h1 of h1s){
+    h1.addEventListener('click',function () {
+        console.log(this)
+        this.style.backgroundColor = makeRandColor();
+        h1.style.color = makeRandColor();
+    })
 }
 
 function colorize() {
     this.style.backgroundColor = makeRandColor();
     this.style.color = makeRandColor();
 }
+
+
+
+
+
+
+// const buttons = document.querySelectorAll('button');
+
+// for (let button of buttons) {
+//     button.addEventListener('click', colorize)
+// }
+
+// const h1s = document.querySelectorAll('h1');
+// for (let h1 of h1s) {
+//     h1.addEventListener('click', colorize)
+// }
+
+// function colorize() {
+//     this.style.backgroundColor = makeRandColor();
+//     this.style.color = makeRandColor();
+// }
