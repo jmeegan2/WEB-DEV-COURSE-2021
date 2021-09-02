b53d7ed125fe672e5e7f30b11ceec78ac41ddde0
James Meegan <jmeegan2@owlmail.harford.edu>
2021-09-01 20:54:15 -0400

    random color exercise

--- HTML CSS JS/DOM_Intro_CODE/DOM_Events_CODE/EventBasics/app.js
+++ HTML CSS JS/DOM_Intro_CODE/DOM_Events_CODE/EventBasics/app.js
@@ -37,3 +37,41 @@
 
 // tasButton.addEventListener('click', twist)
 // tasButton.addEventListener('click', shout)
+
+const btn = document.querySelector("#v2");
+
+btn.onclick = function() {
+    console.log("You Clicked Me!!!")
+    console.log("I hope it worked!!!")
+}
+
+function scream(){
+    console.log("AHAHHHH");
+    console.log("Stop TOUCHING ME")
+}
+
+btn.onmouseenter = scream;
+
+document.querySelector('h1').onclick = () => {
+    alert('you clicked the h1!')
+}
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click', function() {
+    alert("CLICKED!")
+})
+
+function twist() {
+    console.log("TWIST!")
+}
+function shout() {
+    console.log("SHOUT")
+}
+
+const tasButton = document.querySelector('#tas');
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+
+tasButton.addEventListener('click', twist) 
+tasButton.addEventListener('click', shout)
